efbed21cc10d55a68e844572bc079e9d5069edbf
cesar.jaquiery <cesar.jaquiery@students.fhnw.ch>
2024-03-21 15:00:00 +0100

    Edit Design

--- Backend/src/main/java/com/example/myproject/repository/Session.java
+++ Backend/src/main/java/com/example/myproject/repository/Session.java
@@ -6,21 +6,25 @@ import javax.persistence.Id;
 @Entity
 public class Session {
 
-		public Integer getID() {
-		return ID;
-	}
-	public void setID(Integer iD) {
-		ID = iD;
-	}
-	public String getToken() {
-		return token;
-	}
-	public void setToken(String token) {
-		this.token = token;
-	}
-		@Id
-		private Integer ID;
-		private String token;
-	
-	
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer iD) {
+        ID = iD;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Id
+    private Integer ID;
+    private String token;
+
+
 }
